d31e68a77febc7f45a90f0a8c9611c2f0523ee75
sanjin <1032628545@qq.com>
2024-05-21 22:25:42 +0800

    在之后的使用中，均应该upgrade为write或者readGuard，而不应该使用basicGuard，后续可以插入一些例如修改了但是使用的是readGuard
    如果使用basicGuard 我们需要手动加锁，否则就会出现问题。Read或WriteGuard是否可以直接集成BasicGuard呢？

--- BufferPool/src/main/java/storage/page/BasicPageGuard.java
+++ BufferPool/src/main/java/storage/page/BasicPageGuard.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 
 /**
  * 是否需要在此类的方法中 pin一个Page，不需要，因为在fetch和new一个新的page的时候就已经pin了
+ * 使用的时候在new之后，应该立即将其update成read和write从而加上读锁和写锁
  */
 public class BasicPageGuard{
     private BufferPoolManager bufferPoolManager;
@@ -20,6 +21,12 @@ public class BasicPageGuard{
     private boolean isDirty = false;
     private static final Logger logger = Logger.getLogger(BasicPageGuard.class.getName());
 
+    /**
+     * 谨慎使用，因为其使用时并未加锁
+     * 创建之后要立即转为read和write
+     * @param poolManager
+     * @param page
+     */
     public BasicPageGuard(BufferPoolManager poolManager, Page page) {
         this.bufferPoolManager = poolManager;
         this.page = page;
@@ -41,35 +48,52 @@ public class BasicPageGuard{
      * per the specification in the writeup.
      * drop之后就不能再使用此类了
      */
-    void drop() {
-        checkIfBasicPageGuardValid();
-        bufferPoolManager.unpinPage(page.getPageId(), isDirty);
-        this.page = null;
-        this.bufferPoolManager = null;
-    }
-
-    <T> void drop(T outerPage) {
-        checkIfBasicPageGuardValid();
-
-        if (isDirty) { //TODO 是否需要这样做
-            try {
-                byte[] updatedData = SerializeUtils.deserialize(outerPage);
-                // invariants: mayUpdatedData.length <= basicPageGuard.page.length(which equal DBConfig.PAGE_SIZE)
-                System.arraycopy(updatedData, 0, this.page.getData(), 0, updatedData.length);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    private void drop() {
+        if (checkIfBasicPageGuardValid()) { //如果不再有效，说明已经被其他guard给释放过了，无需进行处理
+            bufferPoolManager.unpinPage(page.getPageId(), isDirty);
+            this.page = null;
+            this.bufferPoolManager = null;
         }
-
-        bufferPoolManager.unpinPage(page.getPageId(), isDirty);
-        this.page = null;
-        this.bufferPoolManager = null;
     }
+// 不可能通过BasicPageGuard修改数据，所以drop的时候就无需考虑
+//    private  <T> void drop(T outerPage) {
+//        checkIfBasicPageGuardValid();
+//
+//        if (isDirty) { //TODO 是否需要这样做
+//            try {
+//                byte[] updatedData = SerializeUtils.deserialize(outerPage);
+//                // invariants: mayUpdatedData.length <= basicPageGuard.page.length(which equal DBConfig.PAGE_SIZE)
+//                System.arraycopy(updatedData, 0, this.page.getData(), 0, updatedData.length);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        bufferPoolManager.unpinPage(page.getPageId(), isDirty);
+//        this.page = null;
+//        this.bufferPoolManager = null;
+//    }
 
-    private void checkIfBasicPageGuardValid() {
+//    @Override
+//    public void close() {
+//        /**
+//         * 如果通过BasicPageGuard进行AutoCLose时，要保证页没有被修改，如果修改页，就是用WritePageGuard来进行操作。
+//         */
+//        assert !isDirty;
+//        drop();
+//    }
+
+    /**
+     * 保证bufferPoolManager和page不为空
+     * @throws RuntimeException
+     */
+    private boolean checkIfBasicPageGuardValid() {
         if (Objects.isNull(bufferPoolManager) || Objects.isNull(page)) {
             logger.info("此pageGuard已经不可以使用");
+            return false;
+//            throw new RuntimeException("此pageGuard已经不可以使用,此前已经drop过");
         }
+        return true;
     }
 
     private static void checkIfBasicPageGuardValid(BasicPageGuard basicPageGuard) {
@@ -87,8 +111,13 @@ public class BasicPageGuard{
      *
      * @return an upgraded ReadPageGuard
      */
-    ReadPageGuard UpgradeRead() {
-        return null;
+    public ReadPageGuard UpgradeRead() {
+        checkIfBasicPageGuardValid();
+        page.rLatch();
+        ReadPageGuard readPageGuard = new ReadPageGuard(this.bufferPoolManager, this.page);
+        this.page = null;
+        this.bufferPoolManager = null;
+        return readPageGuard;
     }
 
     /** TODO(P1): Add implementation
@@ -100,8 +129,13 @@ public class BasicPageGuard{
      *
      * @return an upgraded WritePageGuard
      */
-    WritePageGuard UpgradeWrite() {
-        return null;
+    public WritePageGuard UpgradeWrite() {
+        checkIfBasicPageGuardValid();
+        page.wLatch();
+        WritePageGuard writePageGuard = new WritePageGuard(this.bufferPoolManager, this.page);
+        this.page = null;
+        this.bufferPoolManager = null;
+        return writePageGuard;
     }
 
     public int getPageId() { return page.getPageId(); }
@@ -110,10 +144,13 @@ public class BasicPageGuard{
      * 返回一个不可更改的数组，目前通过clone实现
      * @return
      */
-    public byte[] getData() { return page.getData().clone(); } //TODO 对于Clone方法的讨论
+    private byte[] getData() { return page.getData().clone(); } //TODO 对于Clone方法的讨论
 
+    private  <T> T As(T mock) {
+        return As(mock, (Class<T>)mock.getClass());
+    }
 
-    public <T> T As(T mock, Class<T> cl) {
+    private <T> T As(T mock, Class<T> cl) {
         byte[] data = getData();
         try { //TODO exception 应该传播出去 还是本层处理？
             return SerializeUtils.serialize(mock, cl, data);
@@ -122,12 +159,22 @@ public class BasicPageGuard{
         }
     }
 
-    public byte[] GetDataMut() {
+    private byte[] GetDataMut() {
         isDirty = true;
         return page.getData();
     }
 
-    public <T> T AsMut(T mock, Class<T> cl) {
+    private  <T> T AsMut(T mock) {
+        byte[] data = GetDataMut();
+        Class<T> cl = (Class<T>) mock.getClass();
+        try {
+            return SerializeUtils.serialize(mock, cl, data);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    protected  <T> T AsMut(T mock, Class<T> cl) {
         byte[] data = GetDataMut();
         try {
             return SerializeUtils.serialize(mock, cl, data);
@@ -135,7 +182,10 @@ public class BasicPageGuard{
             throw new RuntimeException(ex);
         }
     }
-    public static class ReadPageGuard {
+
+
+
+    public static class ReadPageGuard implements AutoCloseable{
         private BasicPageGuard basicPageGuard;
 
         public ReadPageGuard(BufferPoolManager poolManager, Page page) {
@@ -163,17 +213,27 @@ public class BasicPageGuard{
             basicPageGuard.drop();
         }
 
-        int PageId()  { return basicPageGuard.getPageId(); }
+        int getPageId()  { return basicPageGuard.getPageId(); }
 
         byte[] GetData() { return basicPageGuard.getData(); }
+
+        public <T> T As(T mock) {
+            return As(mock, (Class<T>) mock.getClass());
+        }
 
         public <T> T As(T mock, Class<T> cl) {
             return basicPageGuard.As(mock, cl);
         }
+
+        @Override
+        public void close() {
+            drop();
+        }
     }
 
-    public static class WritePageGuard {
+    public static class WritePageGuard implements AutoCloseable{
         private BasicPageGuard basicPageGuard;
+        private SerializablePageData indexPage = null; //配合close机制使用,将转化的IndexPage保存下来，之后drop的时候，就可以直接使用
 
         public WritePageGuard(BufferPoolManager poolManager, Page page) {
             this.basicPageGuard = new BasicPageGuard(poolManager, page);
@@ -192,11 +252,11 @@ public class BasicPageGuard{
          * However, you should think VERY carefully about in which order you
          * want to release these resources.
          */
-        public <T extends SerializablePageData> void drop(T outerPage) {
+        public void drop() {
             checkIfBasicPageGuardValid(basicPageGuard);
-            if (basicPageGuard.isDirty) {
+            if (basicPageGuard.isDirty) { //TODO 将反序列化的数据写回Page中
                 try {
-                    byte[] updatedData = SerializeUtils.deserialize(outerPage);
+                    byte[] updatedData = SerializeUtils.deserialize(indexPage);
                     // invariants: mayUpdatedData.length <= basicPageGuard.page.length(which equal DBConfig.PAGE_SIZE)
                     System.arraycopy(updatedData, 0, basicPageGuard.page.getData(), 0, updatedData.length);
                 } catch (IOException e) {
@@ -208,19 +268,36 @@ public class BasicPageGuard{
             basicPageGuard.drop();
         }
 
-        int PageId()  { return basicPageGuard.getPageId(); }
+        public int getPageId()  { return basicPageGuard.getPageId(); }
 
         byte[] GetDataMut() { return basicPageGuard.GetDataMut(); }
 
-        public <T> T As(T mock, Class<T> cl) {
-            return basicPageGuard.As(mock, cl);
+        private  <T extends SerializablePageData> T As(T mock, Class<T> cl) {
+            if (indexPage != null) {
+                throw new RuntimeException("多次进行转换");
+            }
+            T instance = basicPageGuard.As(mock, cl);
+            this.indexPage = instance;
+            return instance;
         }
 
-        public <T> T AsMut(T mock, Class<T> cl) {
-            return basicPageGuard.AsMut(mock, cl);
+        public <T extends SerializablePageData> T AsMut(T mock) {
+            return AsMut(mock, ( Class<T> ) mock.getClass());
         }
 
+        public <T extends SerializablePageData> T AsMut(T mock, Class<T> cl) {
+            if (indexPage != null) {
+                throw new RuntimeException("多次进行转换");
+            }
+            T instance =  basicPageGuard.AsMut(mock, cl);
+            this.indexPage = instance;
+            return instance;
+        }
 
+        @Override
+        public void close() {
+            drop();
+        }
     }
 
 }
